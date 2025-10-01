f58bc3e0410699c481ed37f144a09199248e4f8c
VSay29 <162121688+VSay29@users.noreply.github.com>
2025-10-01 10:16:37 +0200

    init commit

--- src/main/java/com/example/calculadora/HelloController.java
+++ src/main/java/com/example/calculadora/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.calculadora;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnGo;
+    @FXML
+    private ComboBox<String> cmbBoxOperation;
+    @FXML
+    private TextField txtNum1;
+    @FXML
+    private TextField txtNum2;
+    @FXML
+    private TextField txtResult;
+
+
+
 }
